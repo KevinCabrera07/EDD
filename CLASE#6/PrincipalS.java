102d9c32e60cc492e825833d51438804f9ec3c30
KevinCabrera07 <cabrerak0704@gmail.com>
2026-03-13 04:31:58 +0000

    Actualizado

--- CLASE#6/PrincipalS.java
+++ CLASE#6/PrincipalS.java
@@ -11,6 +11,13 @@ public class PrincipalS {
             energiaContenedores[i] = r.nextInt(101) + 50;
         }
 
+        System.out.println("Energias generadas:");
+        for (int i = 0; i < energiaContenedores.length; i++) {
+            System.out.print(energiaContenedores[i] + " ");
+        }
+        System.out.println("\n");
+
+        // Filtrar múltiplos de 10
         int[] multiplos = new int[12];
         int contador = 0;
 
@@ -22,8 +29,8 @@ public class PrincipalS {
             }
 
         }
-       int[][] mapaCarga = new int[3][3];
 
+        int[][] mapaCarga = new int[3][3];
         int k = 0;
 
         for (int i = 0; i < 3; i++) {
@@ -35,12 +42,11 @@ public class PrincipalS {
                 } else {
                     mapaCarga[i][j] = -1;
                 }
-                
 
             }
         }
 
-     System.out.println("Mapa de carga:");
+        System.out.println("Mapa de carga:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(mapaCarga[i][j] + " ");
@@ -48,16 +54,15 @@ public class PrincipalS {
             System.out.println();
         }
 
-Suministro[] manifiesto = new Suministro[9];
+        Suministro[] manifiesto = new Suministro[9];
+        int pos = 0;
 
- int pos=0;
- for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
 
                 if (mapaCarga[i][j] != -1) {
 
                     int energia = mapaCarga[i][j];
-
                     String prioridad;
 
                     if (energia > 100) {
@@ -81,6 +86,21 @@ Suministro[] manifiesto = new Suministro[9];
             }
         }
 
-        
+        System.out.println("\nManifiesto de vuelo:");
+
+        for (int i = 0; i < manifiesto.length; i++) {
+
+            if (manifiesto[i] != null) {
+                System.out.println(
+                        manifiesto[i].id + " | Energia: " +
+                        manifiesto[i].nivelEnergia + " | Prioridad: " +
+                        manifiesto[i].prioridad
+                );
+            } else {
+                System.out.println("NULL");
+            }
+
+        }
+
     }
 }
